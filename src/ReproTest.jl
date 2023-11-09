5b70dce0880ed779c4e7f7b0eb55cab6211dff07
Kristoffer <kcarlsson89@gmail.com>
2023-11-09 10:53:38 +0100

    add some debug output

--- src/ReproTest.jl
+++ src/ReproTest.jl
@@ -1,45 +1,31 @@
 module ReproTest
 
 const liblapack = Base.liblapack_name
-import LinearAlgebra: BLAS.@blasfunc, BlasInt
+import LinearAlgebra: LinearAlgebra, BLAS, BLAS.@blasfunc, BlasInt
+using Libdl
 
-## Tools to compute and apply elementary reflectors
-for (larfg, elty) in
-    ((:dlarfg_, Float64),
-     (:slarfg_, Float32),
-     (:zlarfg_, ComplexF64),
-     (:clarfg_, ComplexF32))
-    @eval begin
-        #
-        #    larfg!(x) -> (τ, β)
-        #
-        # Wrapper to LAPACK function family _LARFG.F to compute the parameters
-        # v and τ of a Householder reflector H = I - τ*v*v' which annihilates
-        # the N-1 trailing elements of x and to provide τ and β, where β is the
-        # first element of the transformed vector H*x. The vector v has its first
-        # component set to 1 and is returned in x.
-        #
-        #        .. Scalar Arguments ..
-        #        INTEGER            incx, n
-        #        DOUBLE PRECISION   alpha, tau
-        #        ..
-        #        .. Array Arguments ..
-        #        DOUBLE PRECISION   x( * )
-        function larfg!(x::AbstractVector{$elty})
-            N    = BlasInt(length(x))
-            incx = stride(x, 1)
-            τ    = Ref{$elty}(0)
-            ccall((@blasfunc($larfg), liblapack), Cvoid,
-                (Ref{BlasInt}, Ptr{$elty}, Ptr{$elty}, Ref{BlasInt}, Ref{$elty}),
-                N, x, pointer(x, 2), incx, τ)
-            β = x[1]
-            @inbounds x[1] = one($elty)
-            return τ[], β
-        end
-    end
+@show @blasfunc(dlarfg_)
+@show BLAS.vendor()
+@show BLAS.get_num_threads()
+@show LinearAlgebra.BLAS.lbt_get_config()
+@show Libdl.dlpath(liblapack)
+lapack_ptr = Libdl.dlopen(liblapack)
+@show Libdl.dlsym(lapack_ptr, @blasfunc(dlarfg_))
+
+function larfg!(x::AbstractVector{Float64})
+    N    = BlasInt(length(x))
+    incx = stride(x, 1)
+    τ    = Ref{Float64}(0)
+    ccall((@blasfunc(dlarfg_), liblapack), Cvoid,
+        (Ref{BlasInt}, Ptr{Float64}, Ptr{Float64}, Ref{BlasInt}, Ref{Float64}),
+        N, x, pointer(x, 2), incx, τ)
+    β = x[1]
+    @inbounds x[1] = one(Float64)
+    return τ[], β
 end
 
-ReproTest.larfg!(rand(100))
+
+@show ReproTest.larfg!(rand(100))
 
 
 end # module ReproTest
